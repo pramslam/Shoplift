7254207979892af6db2312ffc813d0ece5f03f2a
pramslam <pramslam@gmail.com>
2018-10-27 18:01:47 -0700

    Updated Grabber, added Hand manager
    Handling manages hand functions
    Grabber correctly detects item collisions and ignores others
    Grabbing an item now picks up an item and destroys world item.

--- Assets/Scripts/GrabberMaster.cs
+++ Assets/Scripts/GrabberMaster.cs
@@ -4,54 +4,68 @@ using UnityEngine;
 
 public class GrabberMaster : MonoBehaviour {
 
-    float castDistance = 1;
-    float forwardOffset = 0.25f;
+    private float castDistance = 1.1f;
+    private float originOffset = 0.25f;             // Offsets the origin of raycast
+    private Handling hand;
 
     public GameObject raycastObject;
-    
-	// Update is called once per frame
-	void Update () {
 
+    void Start()
+    {
+        hand = GameObject.FindObjectOfType<Handling>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
         if (Input.GetMouseButtonDown(0))
             Grab();
 
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonDown(1))
             Release();
+        if (Input.GetKeyDown("e"))
+            hand.Stow();
     }
 
     // Grab an item
     void Grab()
     {
         CheckForHit();
-        // Place item in hand
-        // Stow item in Inventory
-
-        //Debug.Log("Grab");
     }
 
     // Release an item
     void Release()
     {
-        // Place item in world
-
-        //Debug.Log("Release");
+        Debug.Log(hand.GetItems() + " item(s) held.");
     }
-    
+
     void CheckForHit()
     {
         RaycastHit hit;
-        Vector3 offsetTransform = raycastObject.transform.position + (raycastObject.transform.forward * forwardOffset);         // Offsets the raycast start position forward, prevents Raycast from hitting Player Controller
+        Vector3 origin = raycastObject.transform.position + (raycastObject.transform.forward * originOffset);                   // Offsets the raycast start position forward, prevents Raycast from colliding with Player Controller
         Vector3 forward = raycastObject.transform.TransformDirection(Vector3.forward) * castDistance;                           // Get forward direction
+        Ray ray = new Ray(origin, forward);
 
-        if (Physics.Raycast(offsetTransform, forward, out hit, castDistance))
+        if (Physics.Raycast(ray, out hit, castDistance))
         {
-            Debug.DrawRay(offsetTransform, forward * hit.distance, Color.red, 10f, false);                                          // Draw debug ray
-            Debug.Log("Hit " + hit.collider);
+            if (hit.collider.gameObject.tag == "Item")
+            {
+                // Hits an item
+                hand.AddItem(hit);
+                Debug.DrawRay(ray.origin, ray.direction, Color.red, 10f, false);                // Draw debug ray
+            }
+            else
+            {
+                // Hits object but not an item
+                Debug.Log("Did not Hit!");
+                Debug.DrawRay(ray.origin, ray.direction, Color.yellow, 10f, false);             // Draw debug ray
+            }
         }
         else
         {
-            Debug.DrawRay(offsetTransform, forward * castDistance, Color.yellow, 10f, false);                                       // Draw debug ray
+            // Hits nothing
             Debug.Log("Did not Hit!");
+            Debug.DrawRay(origin, forward * castDistance, Color.yellow, 10f, false);            // Draw debug ray
         }
     }
 }
---
+++ Assets/Scripts/Handling.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Handling : MonoBehaviour {
+
+    // Tracks items held, temp variable
+    public int itemsHeld = 0;
+
+    // Adds an item to hand
+    public void AddItem(RaycastHit hit)
+    {
+        RaycastHit _hit = hit;
+
+        // Add item to hand
+        itemsHeld++;
+
+        Debug.Log(_hit.collider.name + " picked up.");                                      //Debug Log
+
+        // Remove item from world
+        Destroy(_hit.collider.gameObject);
+    }
+
+    // Returns held items
+    public int GetItems()
+    {
+        return itemsHeld;
+    }
+
+    // Stow a held item
+    public void Stow()
+    {
+        Debug.Log("Stow item(s) held.");
+    }
+}
