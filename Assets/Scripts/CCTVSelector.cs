c619abfe859610ae060f876248e2c7baf3ed39c8
pramslam <pramslam@gmail.com>
2018-11-07 23:27:26 -0800

    CCTV Sweep animations
    Made animations for 30-45-60 angles, and a short vs long sweep for each.
    Connected animator so cameras can be placed and edited on the fly.

---
+++ Assets/Scripts/CCTVSelector.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTVSelector : MonoBehaviour {
+
+    bool doOnce = false;
+    public bool disabled = false;
+    public float speed = 1.0f;
+    public int cameraType = 0;
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (doOnce == false) {
+            Activate();
+            doOnce = true;
+        }
+        
+        if (disabled == true)
+            Deactivate();
+        else
+        if (disabled == false)
+            Activate();
+    }
+
+    void Activate()
+    {
+        animator.SetInteger("Camera Type", cameraType);
+        animator.speed = speed;
+    }
+
+    void Deactivate()
+    {
+        animator.SetInteger("Camera Type", 0);          // Sets to shutdown animation
+    }
+}
