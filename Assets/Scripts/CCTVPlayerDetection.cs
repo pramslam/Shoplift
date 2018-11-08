5d3ad50e6ac2061746e85f9a22959e6f5e973c37
pramslam <pramslam@gmail.com>
2018-11-07 17:01:40 -0800

    cctv camera
    Added cctv camera that detects the player on collision

---
+++ Assets/Scripts/CCTVPlayerDetection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTVPlayerDetection : MonoBehaviour {
+
+    private GameObject player;
+    private LastPlayerSighting lastPlayerSighting;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        lastPlayerSighting = GameObject.FindGameObjectWithTag("GameController").GetComponent<LastPlayerSighting>();
+	}
+
+    void OnTriggerStay(Collider other)    {
+        if (other.gameObject == player)
+        {
+            Vector3 relPlayerPos = player.transform.position - transform.position;
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, relPlayerPos, out hit)) {
+                if (hit.collider.gameObject == player) {
+                    lastPlayerSighting.position = player.transform.position;
+                    Debug.Log(gameObject.name + " Detected " + player);
+                }
+            }
+        }
+    }
+}
